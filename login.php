b739384895633b1f737b4842ae9a801477be2ee5
Sarvesh Kulkarni <sarvesh123305@gmail.com>
2022-03-04 15:35:07 +0530

    Modal added

--- login.php
+++ login.php
@@ -1,4 +1,63 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+     <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <title></title>
+</head>
+<body>
+<!-- Button trigger modal -->
+<!-- Modal -->
+<div class="modal fade" id="LoginSuccess" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Success</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        You have been logged in successfully
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Done</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="LoginFailure" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Failure</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Kytr chukly
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Done</button>
+      </div>
+    </div>
+  </div>
+</div>
 <?php  
+
+
 if(isset($_POST["submit"])){  
 if(!empty($_POST['Email']) && !empty($_POST['Pass'])) {  
     $user=$_POST['Email'];  
@@ -22,19 +81,31 @@ if(!empty($_POST['Email']) && !empty($_POST['Pass'])) {
     $_SESSION['sess_user']=$user;  
     /* Redirect browser */  
     // header("Location: index.php");  
-    echo '<script>alert("Welcome to GP Connect")</script>';
+    // echo '<script>alert("Welcome to GP Connect")</script>';
+    echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#LoginSuccess').modal('show');
+});
+</script>";
     
     }  
     } else {  
     // echo "Invalid username or password!";
-    echo '<script>alert("Invalid username or password!")</script>';
+    // echo '<script>alert("Invalid username or password!")</script>';
+        echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#LoginFailure').modal('show');
+});
+</script>";
 
     }  
   
 } else {  
     // echo "All fields are required!"; 
-    echo '<script>alert("All fields are required")</script>';
+    // echo '<script>alert("All fields are required")</script>';
 
 }  
 }  
 ?>  
+</body>
+</html>
--- register.php
+++ register.php
@@ -1,5 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+     <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+</head>
+<body>
+<div class="modal fade" id="Register" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Success</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Kytr chukly
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Done</button>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
+
 <?php  
-if(isset($_POST["submitreg"])){  
+if(isset($_POST["submitReg"])){  
+    echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#Register').modal('show');
+});
+</script>";
 if(!empty($_POST['NameReg']) && !empty($_POST['EmailReg'])  &&!empty($_POST['Phone_no'])) {  
     $name=$_POST['NameReg'];  
     $email=$_POST['EmailReg'];  
@@ -11,21 +56,32 @@ if(!empty($_POST['NameReg']) && !empty($_POST['EmailReg'])  &&!empty($_POST['Pho
 
       $con=mysqli_connect('localhost','root','',"onetouch"); 
     $query="insert into register (name,email,phone) values('$name','$email','$phone')";  
-    if (mysqli_query($conn, $query)) {
-    alert("Donee");
+    if (mysqli_query($con, $query)) {
+        // echo '<script>alert("Done")</script>' ;
+          echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#Register').modal('show');
+});
+</script>";
 
        
 } else {
-          alert("Sorry");
+        // echo '<script>alert("Sorry")</script>' ;
+     echo "<script type='text/javascript'>
+$(document).ready(function(){
+$('#Register').modal('show');
+});
+</script>";
 }
   
   
 } else {  
     // echo "All fields are required!"; 
+    
     echo '<script>alert("All fields are required")</script>';
 
 }  
-mysqli_close($conn);
+mysqli_close($con);
 
 }  
 ?>  
