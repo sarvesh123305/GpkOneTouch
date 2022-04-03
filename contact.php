15ee74179746f1471e4d13c10b0ab64b5a282a54
ashwin9099 <ankulkarni731@gmail.com>
2022-04-04 01:54:35 +0530

    contact us DB created & send button querry added ~ash

--- contact.php
+++ contact.php
@@ -26,17 +26,12 @@
    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
    <!-- Tweaks for older IEs-->
    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
-      media="screen">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
    <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <!-- body -->
 
@@ -56,8 +51,7 @@
                   <div class="full">
                      <div class="center-desk">
                         <div class="logo">
-                           <a href="index.html" class="titlenav"><img class="responsives" src="images/loka1.jpeg"
-                                 alt="#" height="100px" width="70px"> GPK@OneTouch</a>
+                           <a href="index.html" class="titlenav"><img class="responsives" src="images/loka1.jpeg" alt="#" height="100px" width="70px"> GPK@OneTouch</a>
 
                            <style>
                               .titlenav {
@@ -76,21 +70,19 @@
                </div>
                <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
                   <nav class="navigation navbar navbar-expand-md navbar-dark ">
-                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04"
-                        aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                      </button>
                      <div class="collapse navbar-collapse" id="navbarsExample04">
                         <ul class="navbar-nav mr-auto">
                            <li class="nav-item ">
-                              <a class="nav-link" href="index.php">Home</a>
+                              <a class="nav-link" href="index.html">Home</a>
                            </li>
                            <li class="nav-item">
                               <a class="nav-link" href="about.html">About</a>
                            </li>
 
                            <li class="nav-item active">
-                              
                               <a class="nav-link" href="contact.html">Contact Us</a>
                            </li>
                         </ul>
@@ -115,66 +107,40 @@
       </div>
    </div>
    <!--  contact -->
-   <div class="modal fade" id="Done" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-         <h5 class="modal-title" id="exampleModalLongTitle"><img src="images/Success.png" class="responsive" height="30px" width="30px" />Success</h5>
-                 <button type="button" class="btn-close" aria-label="Close" data-dismiss="modal"></button>
-
-
-      </div>
-      <div class="modal-body">
-       Thank You for your valuable feedback.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Done</button>
-      </div>
-    </div>
-  </div>
-</div>
    <div class="contact">
       <div class="container">
          <div class="row">
             <div class="col-md-6">
-              <form onsubmit="ok()" class="main_form">
+               <form id="request" class="main_form">
                   <div class="row">
                      <div class="col-md-12 ">
-                        <input class="contactus" placeholder="Name" type="type" name="Name"required>
+                        <input class="contactus" placeholder="Name" type="type" name="Name" required>
                      </div>
                      <div class="col-md-12">
                         <input class="contactus" placeholder="Email" type="type" name="Email" required>
                      </div>
                      <div class="col-md-12">
-                        <input class="contactus" placeholder="Phone Number" type="type" name="Phone Number" required>
+                        <input class="contactus" placeholder="Phone Number" type="type" name="Phone" required>
                      </div>
                      <div class="col-md-12">
-                        <textarea class="textarea" placeholder="Feedback" type="type" Message="Name" required></textarea>
+                        <textarea class="textarea" placeholder="Feedback" type="type" name="feedback" required></textarea>
                      </div>
                      <div class="col-md-10">
-                        <button class="send_btn" type="submit" >Send</button>
-                      
+                        <button type="submit" name="btnsend" class="send_btn">Send</button>
+
                      </div>
                   </div>
                </form>
-               <script type="text/javascript">function ok()
-                { 
-
-               $(document).ready(function(){
-              $('#Done').modal('show');
-                });
-               }
-            </script>
             </div>
 
-            <!-- Map -->
+
+
+
             <div class="col-md-4">
                <div class="map_main">
                   <div class="map-responsive">
-                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.773086574556!2d74.25539691486672!3d16.688234688501105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1002e3f732899%3A0xbd46c7b4413d3652!2sGovernment%20Polytechnic!5e0!3m2!1sen!2sin!4v1646937205513!5m2!1sen!2sin"
-                        width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                 </div>
+                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.773086574556!2d74.25539691486672!3d16.688234688501105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1002e3f732899%3A0xbd46c7b4413d3652!2sGovernment%20Polytechnic!5e0!3m2!1sen!2sin!4v1646937205513!5m2!1sen!2sin" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                  </div>
                </div>
             </div>
          </div>
@@ -211,7 +177,7 @@
                <div class="col-md-4">
                   <!--<h3>News letter</h3>-->
                   <form class="bottom_form">
-                    <!--<input class="enter" placeholder="Enter your email" type="text" name="Enter your email">-->
+                     <!--<input class="enter" placeholder="Enter your email" type="text" name="Enter your email">-->
                      <button class="sub_btn">subscribe</button>
                   </form>
                   <ul class="social_icon">
@@ -227,7 +193,7 @@
                <div class="row">
                   <div class="col-md-10 offset-md-1">
                      <p>
-                        © 20 All Rights Reserved.
+                        © 2022 All Rights Reserved.
                      </p>
                   </div>
                </div>
@@ -246,3 +212,29 @@
 </body>
 
 </html>
+
+<?php
+if (isset($_POST["btnsend"])) {
+
+   if (!empty($_POST['Name']) && !empty($_POST['Email'])  && !empty($_POST['Phone']) && !empty($_POST['feedback'])) {
+      $name = $_POST['Name'];
+      $email = $_POST['Email'];
+      $phone = $_POST['Phone'];
+      $feed = $_POST['feedback'];
+
+      $_POST['Name'] = "";
+      $_POST['Email'] = "";
+      $_POST['Phone'] = "";
+      $_POST['feedback'] = "";
+
+      $con = mysqli_connect('localhost', 'root', '', "onetouch");
+      $query = "insert into contact (name,email,phone,feedback) values('$name','$email','$phone','$feed')";
+      if (mysqli_query($con, $query)) {
+         echo '<script>alert("Feedback Sent!")</script>';
+      }
+   }
+}
+// else {
+//    echo '<script>alert("Error!")</script>';
+// }
+?>
